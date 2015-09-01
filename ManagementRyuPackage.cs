337faa5e66d289301fd6fd86cc7e1c0788776f3f
Warty <ItzWarty@gmail.com>
2015-09-01 04:29:53 -0700

    Management.Impl now supports Dargon's Ryu IoC container.

---
+++ ManagementRyuPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Management.Server;
+using Dargon.Ryu;
+
+namespace Dargon.Management {
+   public class ManagementRyuPackage : RyuPackageV1 {
+      public ManagementRyuPackage() {
+         Singleton<ILocalManagementServer, LocalManagementServer>();
+         Singleton<LocalManagementServer>(ryu => {
+            var managementFactory = ryu.Get<ManagementFactoryImpl>();
+            var managementServerConfiguration = ryu.Get<IManagementServerConfiguration>();
+            return managementFactory.CreateServer(managementServerConfiguration);
+         });
+         Singleton<ILocalManagementRegistry, LocalManagementRegistry>();
+         Singleton<LocalManagementRegistry>(ryu => ryu.Get<ILocalManagementServer>());
+         PofContext<ManagementPofContext>();
+      }
+   }
+}
